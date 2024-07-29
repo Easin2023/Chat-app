e59bd8b4900abcf8fb4d4c40b0bc18160a30179e
Easin2023 <mdeasinarafat016456@gmail.com>
2024-07-29 15:34:33 +0600

    added the chat app project

---
+++ src/Data/Data.jsx
@@ -0,0 +1,18 @@
+import { ChatCircleDots, Phone, Users } from "@phosphor-icons/react";
+
+const Nav_Icons = [
+  {
+    index: 0,
+    icon: <ChatCircleDots />,
+  },
+  {
+    index: 1,
+    icon: <Users />,
+  },
+  {
+    index: 2,
+    icon: <Phone />,
+  },
+];
+
+export default Nav_Icons;
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Stack } from "@mui/material";
+import Site_navbar from "../Component/Site_navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Stack>
+        <Site_navbar />
+      </Stack>
+    </div>
+  );
+};
+
+export default Layout;
